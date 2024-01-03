ee842789e0a3b85c9f72f0b99c042d316bd0e6c2
Oskar <oskarox10@gmail.com>
2024-01-03 12:09:56 +0100

    dodanie pierwszej funcji wywoluajcej pokazanie wszystkich infromacji o kartach

---
+++ src/main/java/oskar/cards_review/CardRepository.java
@@ -0,0 +1,22 @@
+package oskar.cards_review;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class CardRepository {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    public List<Card> getAll() {
+        return jdbcTemplate.query("SELECT id, name, overall, position, review FROM cards", BeanPropertyRowMapper.newInstance(Card.class));
+    }
+
+
+}
